6178e1d46476c288451bbe4e75381715304b56e5
Cristian Teran <criletera@gmail.com>
2023-11-19 00:22:22 -0500

    remove while loop in client

--- client/app.py
+++ client/app.py
@@ -9,15 +9,14 @@ import time
 
 def run():
     print("GRPC Client started")
-    while True:
-        print("GRPC Client waiting 10 second")
-        time.sleep(10)
-        print("GRPC Client sending request to server")
-        with grpc.insecure_channel("server:50051") as channel:
-            stub = users_pb2_grpc.UsersStub(channel)
-            request = users_pb2.GetUsersResponse()
-            response = stub.GetUsers(request)
-        print("GRPC Client message received:\n", response.user)
+    print("GRPC Client waiting 10 second")
+    time.sleep(10)
+    print("GRPC Client sending request to server")
+    with grpc.insecure_channel("server:50051") as channel:
+        stub = users_pb2_grpc.UsersStub(channel)
+        request = users_pb2.GetUsersResponse()
+        response = stub.GetUsers(request)
+    print("GRPC Client message received:\n", response.user)
 
 
 if __name__ == "__main__":
