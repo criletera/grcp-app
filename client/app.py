d5d91b81209c6e46799634e9e9aeac3cc4d44be8
Cristian Teran <criletera@gmail.com>
2023-11-19 00:46:59 -0500

    change server hostname

--- client/app.py
+++ client/app.py
@@ -9,14 +9,15 @@ import time
 
 def run():
     print("GRPC Client started")
-    print("GRPC Client waiting 10 second")
-    time.sleep(10)
-    print("GRPC Client sending request to server")
-    with grpc.insecure_channel("server:50051") as channel:
-        stub = users_pb2_grpc.UsersStub(channel)
-        request = users_pb2.GetUsersResponse()
-        response = stub.GetUsers(request)
-    print("GRPC Client message received:\n", response.user)
+    while True:
+        print("GRPC Client waiting 10 second")
+        time.sleep(10)
+        print("GRPC Client sending request to server")
+        with grpc.insecure_channel("grpc-server-service:50051") as channel:
+            stub = users_pb2_grpc.UsersStub(channel)
+            request = users_pb2.GetUsersResponse()
+            response = stub.GetUsers(request)
+        print("GRPC Client message received:\n", response.user)
 
 
 if __name__ == "__main__":
